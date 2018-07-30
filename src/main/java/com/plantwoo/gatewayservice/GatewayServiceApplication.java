62592938adee81a73fc6115e728408e149728c40
EU\kpaulpa <sundarapandiank@yahoo.com>
2018-07-30 12:48:17 +0530

    gateway changes.

--- src/main/java/com/plantwoo/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/plantwoo/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,18 @@ package com.plantwoo.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
